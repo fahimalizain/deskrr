415d225a28342ddded316ec34ad4791b1196d03e
Preetham Goud <preethamsureshgoud@gmail.com>
2019-01-14 19:54:10 +0530

    login signup

--- cloud-functions/index.js
+++ cloud-functions/index.js
@@ -1,19 +1,72 @@
 const datastore = require("@google-cloud/datastore")({
     projectId: "deskrr"
 })
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
 
 exports.signup = (req, res) => {
-    datastore.save({
-        key: datastore.key("User"),
-        data: {
-            email: req.body.email,
-            password: req.body.password,
-            lastName: req.body.lastName,
-            firstName: req.body.firstName
-        }
-    }).then(() => {
-        res.send("OK");
-    }).catch(() => {
-        res.status(400).send("Failed saving data");
-    })
+
+    // check for valid fields
+    if (!req.body || !req.body.email || !req.body.password || !req.body.lastName || !req.body.firstName) {
+        res.sendStatus(400);
+        return;
+    }
+
+    // building queries
+    const query = datastore.createQuery("User").filter('email', req.body.email);
+    datastore.runQuery(query)
+        .then((data) => {
+            if (data && data.length > 0 && data[0].length > 0) {
+                return Promise.reject("email-exists");
+            }
+        }).then(() => {
+
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hash = bcrypt.hashSync(req.body.password, salt);
+
+            return datastore.save({
+                key: datastore.key("User"),
+                data: {
+                    email: req.body.email,
+                    password: hash,
+                    lastName: req.body.lastName,
+                    firstName: req.body.firstName,
+                    salt: salt
+                }
+            });
+        }).then(() => {
+            res.send("OK");
+        }).catch((e) => {
+            res.status(400).send(e || "Failed saving data");
+        })
+}
+
+exports.login = (req,res) =>{
+    if (!req.body || !req.body.email || !req.body.password){
+        res.sendStatus(400);
+        return;
+    }
+
+    const query = datastore.createQuery('User').filter('email', req.body.email);
+    datastore.runQuery(query)
+        .then((data) => {
+            if (data && data.length > 0 && data[0].length > 0){
+                return data[0][0];
+            }
+            else
+                return Promise.reject('User not Present');
+        }).then((user) => {
+            console.log(user);
+            const salt = user.salt;
+            const hash = bcrypt.hashSync(req.body.password,salt);
+            if(bcrypt.compareSync(user.password,hash)) {
+                return;
+            } else {
+                return Promise.reject("Wrong credentials");
+            }            
+        }).then(() => {
+            res.send("OK");
+        }).catch((e) => {
+            res.status(400).send(e || "Failed login");
+        })
 }
